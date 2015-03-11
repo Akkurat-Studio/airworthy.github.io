415a39ea29cb453e0910f3fe8244cdac53ba423f
Micah <micahmartin@gmail.com>
2015-03-11 15:57:41 -0500

    adds spinner

--- aw.js
+++ aw.js
@@ -13,7 +13,7 @@ function isEmail(email) {
 //{result: "error", msg: "micah@airworthy.co is already subscribed to list Airworthy Newsletter. <a href="http://airworthy.us10.list-manage.com/subscribe/send-email?u=4cc3eaaba999c1ae3aff5aa1e&id=3bf0070305&e=bWljYWhAYWlyd29ydGh5LmNv">Click here to update your profile.</a>"}
 //{result: "success" msg: "Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you."}
 function formSubmitted(resp) {
-  console.log(resp);
+  $("#spinner").detach();
   if("error" == resp.result) {
     $("#email-pocky").append("<div id=\"form-error\" class=\"error\">" + resp.msg + "</div>");
   }
@@ -32,6 +32,7 @@ function submitForm() {
     contentType: "application/json; charset=utf-8",
     success: formSubmitted
   };
+  $("form#subscribe-form").append("<div class=\"spinner\" id=\"spinner\"/>");
   $("form#subscribe-form").ajaxSubmit(options);
 }
 
